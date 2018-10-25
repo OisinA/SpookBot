38e4821b51aec0e26fbff6363be1907e707a2d02
GoremanIX <jansen.arthan@gmail.com>
2018-10-25 18:40:10 +0100

    added doot command

---
+++ dootCommand.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	discord "github.com/bwmarrin/discordgo"
+)
+
+func DootCommand(s *discord.Session, m *discord.MessageCreate, message string) {
+	if m.Author.Bot {
+		return
+	}
+	f, err := os.Open("doot.gif")
+	if err != nil {
+		fmt.Println("Error reading file")
+		return
+	}
+	defer f.Close()
+	ms := &discord.MessageSend{
+		Embed: &discord.MessageEmbed{
+			Image: &discord.MessageEmbedImage{
+				URL: "attachment://" + "doot.gif",
+			},
+		},
+		Files: []*discord.File{
+			&discord.File{
+				Name:   "doot.gif",
+				Reader: f,
+			},
+		},
+	}
+	s.ChannelMessageSendComplex(m.ChannelID, ms)
+}
