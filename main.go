95fc829251d1b7172da165a06fa73b7182307c0d
OisinA <oisinaylward@gmail.com>
2018-10-25 18:34:51 +0100

    Added main stuff.

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	discord "github.com/bwmarrin/discordgo"
+)
+
+var session *discord.Session
+
+func main() {
+	var err error
+	session, err := discord.New("Bot " + ReadToken())
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer session.Close()
+
+	err = session.Open()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = session.UpdateStatus(0, "*aaaaaahhhh*")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, syscall.SIGSEGV, syscall.SIGHUP)
+	<-sc
+}
+
+func ReadToken() string {
+	file := "token"
+	dat, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+	return string(dat)
+}
